0b5857a05602e38b006a7fe19f4994295052b1c7
harsha <manyamharsha112@gmail.com>
2021-05-24 19:07:32 +0530

    phase1

--- src/App.js
+++ src/App.js
@@ -21,11 +21,3 @@ const App = () => {
 
 export default App;
 
-
-// https://developers.google.com/calendar/v3/reference/calendarList/list
-// https://developers.google.com/calendar/v3/reference/events/list
-// https://developers.google.com/calendar/v3/reference/calendarList/list?apix=true#try-it
-// https://github.com/Kubessandra/react-google-calendar-api/blob/master/src/ApiCalendar.js#L213
-// https://www.npmjs.com/package/react-google-calendar-api
-// https://codesandbox.io/s/googlecalendar-woco4?file=/src/test.js
-// https://stackoverflow.com/questions/42626188/styling-a-checkbox-in-a-reactjs-environment
